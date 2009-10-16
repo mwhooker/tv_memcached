6011c5fd62c9b69f93669c9250012532dc480267
Matthew Hooker <mhooker@mhookerosx.cnet.cnwk>
2009-10-16 15:32:49 -0700

    bringing over from tv.com repo

--- Factory.php
+++ Factory.php
@@ -6,34 +6,25 @@ class TV_Memcached_Factory {
 
 	private static $_instances = array();
 
-	public static function getTest($ports) {
-		for ($i = 0; $i < $ports; ++$i) {
-			$arr[] = array('localhost', 11211+$i);
-		}
-		return new TV_Memcached($arr, 'test');
-	}
-
-	public static function get($environment) {
+	public static function get($environment = 'default') {
 
 		if (!isset(self::$_instances[$environment])) {
-			$serverList = self::getServerList($environment);
-			$mcd = new TV_Memcached($serverList, $environment);
+			$serverList = self::getServerList();
+            $mcd = new TV_Memcached($serverList, $environment);
 			self::$_instances[$environment] = $mcd;
 		}
 		return self::$_instances[$environment];
 	}
 
-	private static function getServerList($env) {
-		$arr = array();
-
-		switch ($env) {
-		case 'dev':
-			for ($i = 11211; $i <= 11243; ++$i) {
-				$arr[] = array('localhost', $i);
-			}
-			return $arr;
-		default:
-			return array();
-		}
+	private static function getServerList() {
+        $memcache_list_file = $GLOBALS['GNE_SETTINGS']['GNE']['MEMCACHED_LIST'];
+        
+        $mcd_list = file_get_contents($memcache_list_file);
+        $servers=explode(",", str_replace("'",'', $mcd_list));
+        foreach ($servers as $k=>$v) {
+            $servers[$k] = explode(':', trim($v));
+        }
+        return $servers;
 	}
+    
 }
--- Memcached.php
+++ Memcached.php
@@ -2,6 +2,12 @@
 
 class TV_Memcached extends Memcached {
 
+    protected $pre_expire_last_mcd_result = false;
+	protected $num_servers = NULL;
+	public $perfmon_enabled = false;
+
+	const DEFAULT_TTL = 3600;
+
 	public function __construct(array $serverList, $prefix = '') {
 		parent::__construct();
 
@@ -15,10 +21,8 @@ class TV_Memcached extends Memcached {
 		
 		$this->setOption(Memcached::OPT_LIBKETAMA_COMPATIBLE, true);
 		
-		//$this->setOption(Memcached::OPT_DISTRIBUTION, 
-		//	Memcached::DISTRIBUTION_CONSISTENT);
-		$this->setOption(Memcached::OPT_DISTRIBUTION,
-			Memcached::DISTRIBUTION_MODULA);
+		$this->setOption(Memcached::OPT_DISTRIBUTION, 
+			Memcached::DISTRIBUTION_CONSISTENT);
 		
 		//Enables asynchronous I/O.
 		$this->setOption(Memcached::OPT_NO_BLOCK, true);
@@ -40,9 +44,204 @@ class TV_Memcached extends Memcached {
 				Memcached::SERIALIZER_IGBINARY);
 		}
 
+		$this->num_servers = count($serverList);
 		//@todo if this fails, log it/emit warning.
 		$this->addServers($serverList);
 	}
+
+	public function getNumServers() {
+		return $this->num_servers;
+	}
+	
+    /**
+     * retrieve the specified key from cache. 
+     *
+     * Provides two layers of caching:
+     *  1. local in-memory cache
+     *  2. Memcached
+     * Logs the results to PQP.
+     * To prevent stampedes, it provides a probabalistic timeout mechanism.
+     * Call GNE::mcd_res_ok to see if a valid result was
+     * returned.
+     * ref: http://lists.danga.com/pipermail/memcached/2007-July/004807.html
+     * There is a slight race condition if two clients try to expire the cache
+     * val at the same time, but the CAS logic will prevent one from clobbering
+     * the other. The DB load should be insignificant with our current traffic
+     * profile.
+     *
+     * @author Matthew Hooker <mwhooker@gmail.com>
+     * @param $key string cache key. no spaces allowed.
+     * @return stored value or false on error. 
+     */
+	public function tvGet($key) {
+        $pqp_enabled = class_exists("PQP_Console") && PQP_Console::isEnabled();
+        if ($pqp_enabled) {
+            $start = microtime(true);
+        }
+        
+        // Try fetching from the registry first 
+        $reg = GNE_Registry::getInstance();
+        
+        if (gne_is_cnet()) {
+            if (isset($_REQUEST['clear_cache']) 
+                || isset($_REQUEST['del_cache']) 
+                || isset($_REQUEST['del_mc']) 
+                || isset($_REQUEST['delmc'])) {
+                $this->delete($key);
+                if ($reg->hasKey($key)) {
+                    $reg->remove($key);
+                }
+            }
+        }
+        
+        if ($reg->hasKey($key)) {
+            
+            $data = $reg->get($key);
+            
+            // Log in PQP that the item was found in the Registry
+            if ($pqp_enabled) {
+                PQP_Console::logCache($key, "registry", $start, $data);
+            }
+            return $data;
+        } else {
+            
+            $result = $this->get($key, null, $cas);
+			
+            //reset pre-expire token when we retrieve a new value
+            $this->pre_expire_last_mcd_result = false;
+            
+            if ($this->getResultCode() == Memcached::RES_SUCCESS) {
+                //store the cas token to registry. we may need it.
+                $reg->set('cas_ctrl:' . $key, $cas);
+                
+				//key expires
+                if ($result['ttl'] > 0) {
+                
+					//how much time is left until value expires
+					$time_delta = $result['expire_on'] - time();
+
+					//what percentage are we to reaching the TTL?
+					$normal_delta = ($time_delta / $result['ttl']) * 100;
+
+					//the probability of clearing the cache goes to 100 
+					//as the delta approaches 0
+					$probability = round(100 / (abs($normal_delta) + 1));
+					$clear_value = (mt_rand(1, 100) <= $probability);
+
+					//if it already expired or it's turn is up, expire it.
+					if ($time_delta < 0 || $clear_value) {
+						$this->preExpireLastRes();
+						return false;
+					}
+				}
+                
+                $data = $result['value'];
+                if ($pqp_enabled) {
+                    // Report to PQP a cache hit in memcached
+                    PQP_Console::logCache($key, "memcached", $start, $data);
+                }
+                
+                $reg->set($key, $data);
+                return $data;
+            }
+        }
+        
+        if ($pqp_enabled) {
+            // Report cache miss to PQP
+            PQP_Console::logCache($key, "miss", $start);
+        }
+        return false;
+	}
+
+    /**
+     * set_cache store the provided value to cache
+     *
+     * Provides two layers of caching:
+     *  1. local in-memory cache
+     *  2. Memcached
+     *
+     * 
+     * @author Matthew Hooker <mwhooker@gmail.com>
+     * @param string $key 
+     * @param mixed $value 
+     * @param int $ttl 
+     * @access public
+     * @return bool
+     */
+	public function tvSet($key, $value, $ttl = self::DEFAULT_TTL) {
+		//ttl shouldn't be nagative
+		if (intval($ttl) < 0) {
+			self::DEFAULT_TTL;
+		}
+        //wrap value in the below tuple to facilitate pre-expirey
+        $mcd_value = array(
+            'value' => $value, 
+            'expire_on' => time() + $ttl, 
+            'ttl' => intval($ttl));
+        
+        //this is supposed to estimate our key distribution.
+        //@todo re-enable this using Memcached::getServerByKey
+        if ($this->perfmon_enabled && false && $this->getNumServers() > 0) {
+            $probe = PerfMon::getProbe('cache');
+            
+            $probe->setMessage(array(
+                "key" => $key, 
+                "assigned_server" => ((crc32($key) >> 16) & 0x7fff) % $this->num_servers, 
+                "payload_size" => strlen(serialize($value))));
+        }
+        
+        // Add to Registry
+        $reg = GNE_Registry::getInstance();
+        $reg->set($key, $mcd_value['value']);
+        
+        //if there's a cas value, we're re-setting the value after a pre-expirey
+        $cas_key = 'cas_ctrl:' . $key;
+        if (!$reg->hasKey($cas_key)) {
+            // we set expirey to 24 hours because we manage expiration internally
+            return $this->set($key, $mcd_value, 86400);
+        } else {
+            //if we've got a cas token, use it in update to prevent clobbering.
+            $cas_token = $reg->get($cas_key);
+            $reg->remove($cas_key);
+            return $this->cas($cas_token, $key, $mcd_value, 86400);
+        }
+	}
+
+    /**
+     * check to see if key exists
+     *
+     * @param mixed $key
+     * @access public
+     * @return bool
+     */
+    public function keyExists($key)
+    {
+        if ($this->get_cache($key) == false 
+            && $this->getResultCode() == Memcached::RES_NOTFOUND) {
+            return false;
+        } else 
+            if ($this->getResultCode() == Memcached::RES_SUCCESS) {
+                return true;
+            } else {
+                //possibly throw error
+                return false;
+            }
+	}
+
+    public function lastResFound() {
+        if ($this->pre_expire_last_mcd_result) {
+            return false;
+        }
+        return ($this->getResultCode() == Memcached::RES_SUCCESS);
+    }
+
+    protected function preExpireLastRes()
+    {
+        if ($this->getResultCode() == Memcached::RES_SUCCESS) {
+            $this->pre_expire_last_mcd_result = true;
+        }
+    }
+
 }
 
 
